70e23064fe36d2095af70722ade8cc5ec44475bd
Scott Key <scottdkey@gmail.com>
2020-03-07 16:50:41 -0700

    seed update

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
-     devise_for :users 
-    namespace :admin do
-      resources :users
-      resources :movies
-      resources :comments
-
-      root to: "users#index"
-    end
+    devise_for :users 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
    root to: "movies#index"
 
