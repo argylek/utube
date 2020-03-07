5ddbbf9004ad6ef91dec6b095f1d9c7661c9cfef
Scott Key <scottdkey@gmail.com>
2020-03-07 12:08:45 -0700

    setting up forms

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,33 @@
 class CommentsController < ApplicationController
+  before_action :set_user
+  before_action :set_movie, only: [:new, :edit, :create, :update]
+  before_action :set_comment, only: [:edit, :update]
   def index
+    @comments = current_user.comments
   end
 
   def show
+    @comment = current_user.comments.find(params[:id])
   end
 
   def new
   end
 
   def edit
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
+  end
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+  def comment_prams
+    params.require(:comment).permit(:title, :body)
   end
 end
